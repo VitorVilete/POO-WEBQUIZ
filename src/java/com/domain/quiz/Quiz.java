d506d6b0ae76d46b9d86a5a08bc4c8405605c461
VictorDGon <victordias.gon@gmail.com>
2016-11-15 16:48:22 -0200

    Add files via upload
    com as 10 questoes

--- src/java/com/domain/quiz/Quiz.java
+++ src/java/com/domain/quiz/Quiz.java
@@ -1,6 +1,7 @@
 package com.domain.quiz;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Quiz {
     private int testsCount = 0;
@@ -12,18 +13,37 @@ public class Quiz {
         if(test == null){
             test = new ArrayList<>();
             Question q1 = new Question
-            ("1+1?", "2", new String[]{"0","2","1"});
+            ("Em que cidade fica a fatec-pg?", "Praia Grande", new String[]{"Praia Grande","Santos","Guarujá"});
             test.add(q1);
             Question q2 = new Question
-            ("1+2?", "3", new String[]{"3","2","1"});
+            ("Quem da aula de POO?", "Pupo", new String[]{"Renata","Fabio","Pupo"});
             test.add(q2);
             Question q3 = new Question
-            ("3+4?", "7", new String[]{"7","1","12"});
+            ("Qual o horário para o intervalo?", "20:40", new String[]{"20:40","21:00","20:50"});
             test.add(q3);
             Question q4 = new Question
-            ("2+3?", "5", new String[]{"3","2","5"});
+            ("Quem da aula de Engª Software III?", "Renata", new String[]{"Salgado","Renata","Não tem essa aula"});
             test.add(q4);
+            Question q5 = new Question
+            ("Que horas começa o periodo noturno?", "19:00", new String[]{"19:10","19:00","19:30"});
+            test.add(q5);
+            Question q6 = new Question
+            ("Quem da aula de LP4?", "Cândido", new String[]{"Cândido","Fabio","Priscila"});
+            test.add(q6);
+            Question q7 = new Question
+            ("Qual o do bar/lanchonete que bomba do lado da fatec?", "Malu", new String[]{"Não tem","Novinho","Malu"});
+            test.add(q7);
+            Question q8 = new Question
+            ("Quem da aula de SO II?", "Fabio", new String[]{"Fabio","Wagner","Não tem essa aula"});
+            test.add(q8);
+            Question q9 = new Question
+            ("Qual horas termina o periodo noturno?", "22:30", new String[]{"22:40","22:20","22:30"});
+            test.add(q9);
+            Question q10 = new Question
+            ("Quantos de curso é ADS?", "3 anos", new String[]{"1 e meio","2 anos","3 anos"});
+            test.add(q10);
         }
+        Collections.shuffle(test);
         return test;
     }
     
